608ed91189c86f60bad70f2a43338b34045507a1
tanhanzong <hanzongtan5018@gmail.com>
2019-07-24 11:03:02 +0800

    Initial Commit

--- app/src/main/java/my/edu/tarc/bankingapp/LoginActivity.java
+++ app/src/main/java/my/edu/tarc/bankingapp/LoginActivity.java
@@ -4,13 +4,21 @@ import android.content.Intent;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity{
     Button btnNext;
+    TextView txtViewReg;
+    EditText editTextUsername;
+    RadioButton radioButtonYes, radioButtonNo;
+    EditText editTextPw;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,35 +26,64 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         btnNext = findViewById(R.id.btn_next);
-
-        Button btnRegister = findViewById(R.id.btn_register);
+        txtViewReg = findViewById(R.id.textView_reg);
+        editTextUsername = findViewById(R.id.editText_username);
 
         btnNext.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                AlertDialog.Builder mBuilder = new AlertDialog.Builder(LoginActivity.this);
-                View mView = getLayoutInflater().inflate(R.layout.dialog_login, null);
-                Button btnLogin = mView.findViewById(R.id.btn_login);
+                String username = editTextUsername.getText().toString();
 
-                mBuilder.setView(mView);
-                AlertDialog dialog = mBuilder.create();
-                dialog.show();
+                if(username.isEmpty() == true){
+                    Toast.makeText(getApplicationContext(), "Please enter your user ID.", Toast.LENGTH_SHORT).show();
+                }else{
+                    AlertDialog.Builder mBuilder = new AlertDialog.Builder(LoginActivity.this);
+                    final View mView = getLayoutInflater().inflate(R.layout.dialog_login, null);
+                    Button btnLogin = mView.findViewById(R.id.btn_login);
 
-                btnLogin.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Intent intent = new Intent(v.getContext(), MainActivity.class);
-                        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                        startActivity(intent);
-                    }
-                });
+                    mBuilder.setView(mView);
+                    AlertDialog dialog = mBuilder.create();
+                    dialog.show();
+
+                    radioButtonYes = mView.findViewById(R.id.radioBtn_yes);
+                    radioButtonNo = mView.findViewById(R.id.radioBtn_no);
+                    editTextPw = mView.findViewById(R.id.editText_password);
+
+                    radioButtonYes.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            editTextPw.setFocusable(true);
+                            editTextPw.setClickable(true);
+                            editTextPw.setFocusableInTouchMode(true);
+                        }
+                    });
+
+                    radioButtonNo.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            Intent intentLogin = new Intent(v.getContext(), LoginActivity.class);
+                            intentLogin.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                            startActivity(intentLogin);
+                        }
+                    });
+
+                    btnLogin.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            Intent intent = new Intent(v.getContext(), MainActivity.class);
+                            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                            startActivity(intent);
+                        }
+                    });
+                }
+
             }
 
 
 
         });
 
-        btnRegister.setOnClickListener(new View.OnClickListener() {
+        txtViewReg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(v.getContext(), RegisterActivity.class);
@@ -59,6 +96,9 @@ public class LoginActivity extends AppCompatActivity {
 
 
     }
+
+
+
 
 //    public void login(final View view) {
 //        Intent intent = new Intent(view.getContext(), MainActivity.class);
