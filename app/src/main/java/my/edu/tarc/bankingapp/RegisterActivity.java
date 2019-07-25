eb8ea5c0039b974ee53dcad59b3825c94d2fcfff
tanzy1998 <tanzy-wa16@student.tarc.edu.my>
2019-07-25 20:00:08 +0800

    Update register

--- app/src/main/java/my/edu/tarc/bankingapp/RegisterActivity.java
+++ app/src/main/java/my/edu/tarc/bankingapp/RegisterActivity.java
@@ -25,9 +25,9 @@ public class RegisterActivity extends AppCompatActivity {
 
         HorizontalStepView stepView = (HorizontalStepView) findViewById(R.id.step_view);
         List<Step> stepList = new ArrayList<>();
-        stepList.add(new Step("    User\n   Detail", Step.State.CURRENT));
+        stepList.add(new Step("User Login\n   Detail", Step.State.CURRENT));
         stepList.add(new Step("Personal\n Detail"));
-        stepList.add(new Step("Contact\n Detail"));
+        stepList.add(new Step("Emergency\n Contact\n Detail"));
 
         stepView.setSteps(stepList).setTextSize(13)
                 .setCompletedLineColor(ContextCompat.getColor(getBaseContext(), android.R.color.black))//set StepsViewIndicator完成线的颜色
