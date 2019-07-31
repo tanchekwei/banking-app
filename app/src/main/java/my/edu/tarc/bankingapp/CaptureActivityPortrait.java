6e9113337b7dd8c449c55f32eda8b3c4963490ce
jtmycraft <30696041+jtmycraft@users.noreply.github.com>
2019-07-31 09:22:39 +0800

    Updated Scan QR with Portrait

---
+++ app/src/main/java/my/edu/tarc/bankingapp/CaptureActivityPortrait.java
@@ -0,0 +1,11 @@
+package my.edu.tarc.bankingapp;
+
+import com.journeyapps.barcodescanner.CaptureActivity;
+
+/**
+ * This Activity is exactly the same as CaptureActivity, but has a different orientation
+ * setting in AndroidManifest.xml.
+ */
+public class CaptureActivityPortrait extends CaptureActivity {
+
+}
